f2dfbfc16795d923175f84199b179ff5a039bf08
Youssef Ashraf <83988379+Youssef-Ashraf71@users.noreply.github.com>
2022-04-19 21:15:49 +0200

    Add files via upload

---
+++ Codeforces Contests/Educational Codeforces Round 126 (Rated for Div. 2)/C - Water the Trees.cpp
@@ -0,0 +1,87 @@
+/*
+       ____            ___________
+      /\   \          |
+     /  \   \         |
+    /    \   \        |___________
+   /______\   \                  |
+  /        \   \                 |
+ /          \  /       __________|
+/            \/
+
+ ___  ___              F
+|\  \|\   \            F
+\ \  \_\   \           F
+ \ \ ___    \          F
+  \  \ \\ \  \         F
+   \  \__\\ \_\        F
+    \ |__| \|__|       F
+
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);  cout.tie(0);
+using namespace std;
+#define REPi(a,b) for (int i = a; i <= b; i++)
+#define REPj(a,b) for (int j = a; j <= b; j++)
+#define genMod(x,Mod) (x<0)?(x % Mod + Mod) : (x % Mod)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(v) (int) v.size()
+#define cin(vec) for(auto& i : vec) cin >> i;
+#define el   endl
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define coutv(vec) for(auto& i : vec) {cout << i << " " ; cout << "\n";}
+#define couth(vec) for(auto& i : vec) {cout << i << " " ;}
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+const ll N = 1e7,bun = 3, mod = 1e9 + 7;
+ll REMINDER = 1000000007;
+/*
+int n, m;
+char grid[1001][1001];
+int vis[1001][1001];
+1
+int di[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dj[] = {1, -1, 0, 1, -1, 0, 1, -1};
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+*/
+void solve() {
+    ll n, i = 0, j = 0, cnt=0;
+    cin>>n;  ll ans=1e18;
+    vector<ll>x(n); cin(x);  ll mx=0;
+   for (auto &it:x ){
+       mx=max(it,mx);
+   }
+    for(i=mx;i<=mx+3;i++){
+        ll e=0,o=0;
+        for(auto &it :x) {
+            o += (i -it)%2;
+            e += (i-it)/2;
+        }
+        ll tot=o+e*2;  ll d=tot/3*2;
+        if(tot%3==1) {d++;}
+        if(tot%3==2){d+=2; }
+        ans=min(ans,max(o*2-1,d));
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+    fast; // ashf gamed tenen
+    //freopen("window.in", "r", stdin);   // for file reading
+    ll test_case=1 ;     cin >> test_case;
+
+    while (test_case--)
+    {
+        solve();
+    }
+    return 0;
+}
