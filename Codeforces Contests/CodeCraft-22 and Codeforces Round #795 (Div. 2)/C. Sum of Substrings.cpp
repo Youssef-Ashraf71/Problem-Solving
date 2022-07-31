21b5c2eb0a0ca435fb65f023f10e971f90a0186c
Youssef Ashraf <83988379+Youssef-Ashraf71@users.noreply.github.com>
2022-07-31 02:28:57 +0200

    Add files via upload

---
+++ Codeforces Contests/CodeCraft-22 and Codeforces Round #795 (Div. 2)/C. Sum of Substrings.cpp
@@ -0,0 +1,88 @@
+/*
+       ____            ___________
+      /\   \          |
+     /  \   \         |
+    /    \   \        |___________
+   /______\   \                  |
+  /        \   \                 |
+ /          \  /       __________|
+/            \/
+
+ ___  ___              F
+|\  \|\   \            F
+\ \  \_\   \           F
+ \ \ ___    \          F
+  \  \ \\ \  \         F
+   \  \__\\ \_\        F
+    \ |__| \|__|       F
+
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);  cout.tie(0);
+using namespace std;
+#define REPi(a,b) for (int i = a; i <= b; i++)
+#define REPj(a,b) for (int j = a; j <= b; j++)
+#define genMod(x,Mod) (x<0)?(x % Mod + Mod) : (x % Mod)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(v) (int) v.size()
+#define cin(vec) for(auto& i : vec) cin >> i;
+#define el   endl
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) {for(int j = 0; j < m ;j++ ){ cin >> vec[i][j];}  }
+#define coutv(vec) for(auto& i : vec) {cout << i << " " ; cout << "\n";}
+#define couth(vec) for(auto& i : vec) {cout << i << " " ;}
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+using namespace __gnu_pbds;
+typedef tree<pair<ll,ll>,null_type,less<>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+const ll N = 1e7,bun = 3, mod = 1e9 + 7;
+const int oo= 0x3f3f3f3f;
+ll REMINDER = 1000000007;
+/*
+int n, m;
+char grid[1001][1001];
+int vis[1001][1001];
+1
+int di[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dj[] = {1, -1, 0, 1, -1, 0, 1, -1};
+ bool ok(int x, int y,int n, int m) { return x >= 0 && y >= 0 && x < n && y < m; }
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+*/
+void solve()
+{
+    ll n,i=0,j=0,cnt=0;
+    ll k,sum=0,tr=1; string s; cin>>n>>k>>s;
+    ll  l,r;  l=r=-1;
+    for (i=0;i<n;i++)
+    {
+        if (s[i]=='1')
+        {
+            sum+=11;
+            if(l==-1) l=i;
+            r=i;
+        }
+    }
+    if (r!=-1 && k+1+r>=n) sum-=10,k-=n-1-r,tr=0;
+    if ((l!=r||tr) && l!=-1 && k>=l) sum-=1;
+    cout<<sum<<endl;
+
+}
+int main()
+{
+    fast; // ashf gamed tenen
+    //freopen("window.in", "r", stdin);   // for file reading
+    ll test_case=1 ;      cin >> test_case;
+    while (test_case--)
+    {
+        solve();
+    }
+    return 0;
+}
