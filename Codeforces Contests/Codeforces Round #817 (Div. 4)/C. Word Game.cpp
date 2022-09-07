703ed53b6653aac8e0514bed1124633bdf19fa06
Youssef Ashraf <83988379+Youssef-Ashraf71@users.noreply.github.com>
2022-09-07 22:12:31 +0200

    Add files via upload

---
+++ Codeforces Contests/Codeforces Round #817 (Div. 4)/C. Word Game.cpp
@@ -0,0 +1,142 @@
+/*
+       ____            ___________
+      /\   \          |
+     /  \   \         |
+    /    \   \        |___________
+   /______\   \                  |
+  /        \   \                 |
+ /          \  /       __________|
+/            \/
+
+ ___  ___              F
+|\  \|\   \            F
+\ \  \_\   \           F
+ \ \ ___    \          F
+  \  \ \\ \  \         F
+   \  \__\\ \_\        F
+    \ |__| \|__|       F
+
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);  cout.tie(0);
+using namespace std;
+#define genMod(x,Mod) (x<0)?(x % Mod + Mod) : (x % Mod)
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define cin(vec) for(auto& i : vec) cin >> i;
+#define el   endl
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) {for(int j = 0; j < m ;j++ ){ cin >> vec[i][j];}  }
+#define coutv(vec) for(auto& i : vec) {cout << i << " " ; cout << "\n";}
+#define couth(vec) for(auto& i : vec) {cout << i << " " ;}
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+using namespace __gnu_pbds;
+typedef tree<pair<ll,ll>,null_type,less<>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+const ll N = 1e7,bun = 3, mod = 1e9 + 7;
+const int oo= 0x3f3f3f3f;
+ll REMINDER = 1000000007;
+/*
+int n, m;
+char grid[1001][1001];
+int vis[1001][1001];
+int di[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dj[] = {1, -1, 0, 1, -1, 0, 1, -1};
+ bool ok(int x, int y,int n, int m) { return x >= 0 && y >= 0 && x < n && y < m; }
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+*/
+void solve()
+{
+    ll n,i=0,j=0,cnt=0;
+    cin>>n;
+    vector<string>x(n),y(n),z(n);
+    cin(x); cin(y); cin(z);
+    map<string , ll >vis1,vis2,vis3;
+    for(i=0;i<n;i++){
+        vis1[x[i]]++; vis2[y[i]]++; vis3[z[i]]++;
+    }
+    ll a1,a2,a3; a1=a2=a3=0;
+    for(i=0;i<n;i++){
+        if(vis1[x[i]] && vis2[x[i]] && vis3[x[i]]){
+            vis1[x[i]]--; vis2[x[i]]--; vis3[x[i]]--;
+        }
+        else if(vis1[x[i]] && vis2[x[i]] ){
+            vis1[x[i]]--; vis2[x[i]]--; a1++; a2++;
+         }
+        else if(vis1[x[i]] && vis3[x[i]] ){
+            vis1[x[i]]--; vis3[x[i]]--;  a1++; a3++;
+        }
+        else if(vis2[x[i]]  && vis3[x[i]]  ){
+            vis2[x[i]]--; vis3[x[i]]--; a2++; a3++;
+        }
+        else if(vis1[x[i]]) vis1[x[i]]--,a1+=3;
+        else if(vis2[x[i]]) vis2[x[i]]--,a2+=3;
+        else if(vis3[x[i]]) vis3[x[i]]--,a3+=3;
+    }
+    for(i=0;i<n;i++){
+        if(vis1[y[i]] && vis2[y[i]] && vis3[y[i]]){
+            vis1[y[i]]--; vis2[y[i]]--; vis3[y[i]]--;
+        }
+        else if(vis1[y[i]] && vis2[y[i]] ){
+            vis1[y[i]]--; vis2[y[i]]--; a1++; a2++;
+        }
+        else if(vis1[y[i]] && vis3[y[i]] ){
+            vis1[y[i]]--; vis3[y[i]]--;  a1++; a3++;
+        }
+        else if(vis2[y[i]]  && vis3[y[i]]  ){
+            vis2[y[i]]--; vis3[y[i]]--; a2++; a3++;
+        }
+        else if(vis1[y[i]]) vis1[y[i]]--,a1+=3;
+        else if(vis2[y[i]]) vis2[y[i]]--,a2+=3;
+        else if(vis3[y[i]]) vis3[y[i]]--,a3+=3;
+    }
+    for(i=0;i<n;i++){
+        if(vis1[z[i]] && vis2[z[i]] && vis3[z[i]]){
+            vis1[z[i]]--; vis2[z[i]]--; vis3[z[i]]--;
+        }
+        else if(vis1[z[i]] && vis2[z[i]] ){
+            vis1[z[i]]--; vis2[z[i]]--; a1++; a2++;
+        }
+        else if(vis1[z[i]] && vis3[z[i]] ){
+            vis1[z[i]]--; vis3[z[i]]--;  a1++; a3++;
+        }
+        else if(vis2[z[i]]  && vis3[z[i]]  ){
+            vis2[z[i]]--; vis3[z[i]]--; a2++; a3++;
+        }
+        else if(vis1[z[i]]) vis1[z[i]]--,a1+=3;
+        else if(vis2[z[i]]) vis2[z[i]]--,a2+=3;
+        else if(vis3[z[i]]) vis3[z[i]]--,a3+=3;
+    }
+    cout<<a1<<" "<<a2<<" "<<a3<<endl;
+
+
+
+
+
+
+
+
+
+
+
+}
+int main()
+{
+    fast; // ashf gamed tenen
+    //freopen("window.in", "r", stdin);   // for file reading
+    ll test_case=1 ;      cin >> test_case;
+    while (test_case--)
+    {
+        solve();
+    }
+    return 0;
+}
